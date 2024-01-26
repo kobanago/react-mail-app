c5a8ee7ead64b3d64d0c9537195dcc053361a9e8
kobanago <kobanago3800@gmail.com>
2024-01-26 11:52:54 +0900

    feat：storybook：add：設定ファイル

---
+++ .storybook/preview.ts
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    decorators: [
+      withThemeFromJSXProvider({
+        Provider: ThemeProvider,
+        GlobalStyles: CssBaseline,
+      }),
+    ],
+  },
+};
+
+export default preview;
