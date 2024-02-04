7375378846cb592f310034cecd8023e49e74de86
kobanago <kobanago3800@gmail.com>
2024-02-04 09:53:01 +0900

    feat：react-router：ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import AuthGoogle from './AuthGoogle';
+import { LoginPage } from './stories/components/pages/LoginPage';
+import { CommonTemplate } from './stories/components/templates/CommonTemplate';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/message' element={<CommonTemplate title='MESSAGE' />} />
+        <Route path='/person' element={<CommonTemplate title='PERSON' />} />
+        <Route path='/auth' element={<AuthGoogle />} />
+      </Routes>
     </>
   );
 }
